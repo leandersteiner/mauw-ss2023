35cad79b7b762424f0595db1702af4c4384af15b
Leander Steiner <leandersteiner@yahoo.de>
2023-05-31 18:27:52 +0200

    #42: Added example for auth api (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { DatePicker } from 'antd';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Overview } from './components/appLayout/Overview';
 import { PathContextProvider } from './context/PathContext';
 import { HomePage } from './components/start/HomePage';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+
+const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
   return (
     <PathContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to='/home' />} />
-          <Route path='/login' element={<Overview />} />
-          <Route path='/home' element={<Overview />}>
-            <Route index element={<HomePage />} />
-            <Route path='boards' element={<DatePicker />} />
-            <Route path='settings' element={<DatePicker />} />
-            <Route path='create-project' element={<DatePicker />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navigate to='/home' />} />
+            <Route path='/home' element={<Overview />}>
+              <Route path='login' element={<Login />} />
+              <Route path='register' element={<Register />} />
+              <Route index element={<HomePage />} />
+              <Route path='boards' element={<DatePicker />} />
+              <Route path='settings' element={<DatePicker />} />
+              <Route path='create-project' element={<DatePicker />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </PathContextProvider>
   );
 };
---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+import { Button, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { AuthResponse, loginUser } from '../../api/authApi';
+
+type LoginFormData = {
+  username: string;
+  password: string;
+};
+
+export const Login = () => {
+  const queryClient = useQueryClient();
+
+  const createLoginMutation = useMutation(loginUser);
+
+  const onFinish = async (data: LoginFormData) => {
+    const { username, password } = data;
+    if (!username || !password) {
+      // Todo: error handling
+      alert('Please provide username and password');
+    }
+    await createLoginMutation.mutate(data, {
+      onSuccess: (response: AuthResponse) => {
+        console.log(response.token);
+        queryClient.invalidateQueries('login');
+        // Todo: set current user show success notification
+      }
+    });
+  };
+
+  return (
+    <Form
+      name='normal_login'
+      className='login-form'
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name='username'
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='Username' />
+      </Form.Item>
+      <Form.Item
+        name='password'
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className='site-form-item-icon' />}
+          type='password'
+          placeholder='Password'
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type='primary' htmlType='submit' className='login-form-button'>
+          Log in
+        </Button>
+        Or <a href='register'>register now!</a>
+      </Form.Item>
+    </Form>
+  );
+};
