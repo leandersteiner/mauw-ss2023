708a5ef5832e6662207b84e78cb047dbd2ae6cd5
Leander Steiner <leandersteiner@yahoo.de>
2023-06-03 19:07:06 +0200

    #34: Updated side menu to correctly show active page

--- src/components/layout/SideMenu.tsx
+++ src/components/layout/SideMenu.tsx
@@ -2,36 +2,39 @@ import { Menu } from 'antd';
 import React, { CSSProperties } from 'react';
 import { Link } from 'react-router-dom';
 import { UserOutlined } from '@ant-design/icons';
+import { usePathContext } from '../../context/PathContext';
+
+const menuWrapperStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%'
+};
+
+const logoStyle: CSSProperties = {
+  height: 32,
+  margin: 16,
+  background: 'rgba(255, 255, 255, 0.2)'
+};
+
+const menuStyle: CSSProperties = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column'
+};
+
+const bottomMenuStyle: CSSProperties = {
+  marginTop: 'auto'
+};
 
 export const SideMenu: React.FC = () => {
-  const menuWrapperStyle: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%'
-  };
-
-  const logoStyle: CSSProperties = {
-    height: 32,
-    margin: 16,
-    background: 'rgba(255, 255, 255, 0.2)'
-  };
-
-  const menuStyle: CSSProperties = {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  };
-
-  const bottomMenuStyle: CSSProperties = {
-    marginTop: 'auto'
-  };
+  const { path } = usePathContext();
 
   return (
     <div style={menuWrapperStyle}>
       <div style={logoStyle} />
-      <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']} style={menuStyle}>
+      <Menu theme='dark' mode='vertical' style={menuStyle} selectedKeys={[path]}>
         <Menu.ItemGroup>
-          <Menu.Item key='1'>
+          <Menu.Item key='home'>
             <Link to='/home'>
               <UserOutlined />
               <span>Home</span>
@@ -39,14 +42,14 @@ export const SideMenu: React.FC = () => {
           </Menu.Item>
         </Menu.ItemGroup>
         <Menu.ItemGroup style={bottomMenuStyle}>
-          <Menu.Item key='2'>
-            <Link to='/auth/login'>
+          <Menu.Item key='login'>
+            <Link to='login'>
               <UserOutlined />
               <span>Login</span>
             </Link>
           </Menu.Item>
-          <Menu.Item key='3'>
-            <Link to='/auth/register'>
+          <Menu.Item key='register'>
+            <Link to='register'>
               <UserOutlined />
               <span>Register</span>
             </Link>
