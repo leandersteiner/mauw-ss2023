df234406e2154a907ee90b042c151812adc88b56
Leander Steiner <leandersteiner@yahoo.de>
2023-06-03 19:07:17 +0200

    #34: Fixed logout

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -5,7 +5,7 @@ import { useAuth } from '../../context/AuthContext';
 export const Logout = () => {
   const { onLogout, token } = useAuth();
 
-  if (token) return <Navigate to='/home' replace />;
+  if (!token) return <Navigate to='/home' replace />;
   onLogout();
   return <Navigate to='/home' replace />;
 };
