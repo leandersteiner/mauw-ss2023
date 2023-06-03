ff3d331bcedb02785b562285ba6d743f62766ebc
Leander Steiner <leandersteiner@yahoo.de>
2023-06-03 18:23:48 +0200

    #34: Added logout component

---
+++ src/components/auth/Logout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+
+export const Logout = () => {
+  const { onLogout, token } = useAuth();
+
+  if (token) return <Navigate to='/home' replace />;
+  onLogout();
+  return <Navigate to='/home' replace />;
+};
