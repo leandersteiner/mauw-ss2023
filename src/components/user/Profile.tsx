b15bece88e14fa030b5b0cd45d5c412893c007da
Leander Steiner <leandersteiner@yahoo.de>
2023-06-09 17:50:00 +0200

    #34: added profile view

--- src/components/user/Profile.tsx
+++ src/components/user/Profile.tsx
@@ -1,40 +1,18 @@
-import { Button, message, Popconfirm } from 'antd';
-import { useMutation } from '@tanstack/react-query';
-import { useNavigate } from 'react-router';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import { deleteUser } from '../../api/userApi';
+import { UpdateUserForm } from './UpdateUserForm';
+import { DeleteAccountButton } from './DeleteAccountButton';
 
 export const Profile = () => {
-  const { user } = useAuth();
-  const { mutate } = useMutation({
-    mutationFn: deleteUser
-  });
-  const navigate = useNavigate();
+  const { user, token, onLogin } = useAuth();
 
-  const confirm = () => {
-    if (user) {
-      mutate(user.id);
-      message.success('Account deleted');
-      navigate('/auth/logout');
-    }
-  };
-
-  const cancel = () => {};
+  if (!user || !token || !onLogin) return <Navigate to='/logout' />;
 
   return (
     <>
       <h1>{`Profile of ${user?.username}`}</h1>
-      <p>{user?.email}</p>
-      <Popconfirm
-        title='Delete the task'
-        description='Are you sure to delete this task?'
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText='Yes'
-        cancelText='No'
-      >
-        <Button danger>Delete</Button>
-      </Popconfirm>
+      <UpdateUserForm token={token} user={user} onLogin={onLogin} />
+      <DeleteAccountButton user={user} />
     </>
   );
 };
