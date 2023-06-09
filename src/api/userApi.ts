8ba62bcd60afac3e124c0b1adb0d6455c6d05839
Leander Steiner <leandersteiner@yahoo.de>
2023-06-09 17:48:24 +0200

    #34: updated userApi

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,12 +1,14 @@
 import { User } from '../models/user/User';
 import { api } from './api';
 
-type UserResponse = User;
+export type UserResponse = User;
+
+type UpdateUserRequest = Partial<User>;
 
 export const getUser = (userId: string) =>
   api.get<UserResponse>(`users/${userId}`).then(res => res.data);
 
-export const updateUser = (userId: string, data: { email: string }) =>
+export const updateUser = (userId: string) => (data: UpdateUserRequest) =>
   api.patch<UserResponse>(`users/${userId}`, data).then(res => res.data);
 
 export const deleteUser = (userId: string) => api.delete(`users/${userId}`);
