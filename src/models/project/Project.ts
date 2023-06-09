d359f2f1d1d56434207c909e0edd83cee3d1ea56
Michael Jungkind <MJungkind@protonmail.com>
2023-06-09 18:02:17 +0200

    #4: Updated Project dtos

--- src/models/project/Project.ts
+++ src/models/project/Project.ts
@@ -1,5 +1,18 @@
+import { Timestamp } from '../../helpers/date';
+
 export interface Project {
-  id: number;
+  id: string;
   name: string;
   private: boolean;
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+}
+
+export interface CreateProjectRequest {
+  name: string;
+  private: boolean;
+}
+
+export interface AddUserToProjectRequest {
+  userId: string;
 }
