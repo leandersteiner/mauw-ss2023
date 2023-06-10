49e2cc70764f8c69773cbcb345dbc8843a155610
Leander Steiner <leandersteiner@yahoo.de>
2023-06-10 20:29:31 +0200

    #19: updated board model

--- src/models/board/Board.ts
+++ src/models/board/Board.ts
@@ -1,4 +1,15 @@
 export interface Board {
   id: string;
   title: string;
+  columns: [
+    {
+      id: string;
+      title: string;
+      state: {
+        id: string;
+        name: string;
+      };
+      tasks: [{ id: string }];
+    }
+  ];
 }
