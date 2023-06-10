e960225b374bacbf0a7789b2158bc73a858a3ed8
Leander Steiner <leandersteiner@yahoo.de>
2023-06-10 20:29:59 +0200

    #19: added data fetching to board component

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,19 +1,37 @@
-import { Col, Row } from 'antd';
-import { BoardColumn, BoardColumnProps } from './BoardColumn';
+import { Col, Row, Space, Spin } from 'antd';
+import { useQuery } from '@tanstack/react-query';
+import { BoardColumn } from './BoardColumn';
+import { BoardResponse, getBoard } from '../../api/boardApi';
 
 type BoardProps = {
-  id: string;
-  name: string;
-  boardColumns: BoardColumnProps[];
+  projectId: string;
 };
 
 export const Board = (props: BoardProps) => {
-  const { boardColumns } = props;
+  const { isLoading, isError, error, data } = useQuery<BoardResponse, Error>({
+    queryKey: ['board', props.projectId],
+    queryFn: () => getBoard(props.projectId)
+  });
+
+  if (isLoading) {
+    return (
+      <Space direction='vertical' style={{ width: '100%' }}>
+        <Spin tip='Loading' size='large'>
+          <div className='content' />
+        </Spin>
+      </Space>
+    );
+  }
+
+  if (isError) {
+    return <div>There was an unexpected error: {error.message}</div>;
+  }
+
   return (
     <Row>
-      {boardColumns.map(column => (
+      {data.columns.map(column => (
         <Col key={column.id} flex='auto'>
-          <BoardColumn id={column.id} title={column.title} />
+          <BoardColumn id={column.id} title={column.title} tasks={column.tasks} />
         </Col>
       ))}
     </Row>
