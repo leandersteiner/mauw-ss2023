f4a638b6cd19053749c1b808bbf7483d015721d4
Leander Steiner <leandersteiner@yahoo.de>
2023-06-20 21:18:52 +0200

    #19: added confirm dialog to task deleting

--- src/components/board/BoardColumnTask.tsx
+++ src/components/board/BoardColumnTask.tsx
@@ -1,13 +1,16 @@
-import { Card, Col, Progress, Row, Space, Tooltip } from 'antd';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Card, Col, Popconfirm, Progress, Row, Space, Tooltip } from 'antd';
+import { DeleteOutlined, EditOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 import { Task } from '../../models/task/Task';
 import { Subtask } from '../../models/task/Subtask';
+import { TaskOverviewModal } from '../task/TaskOverviewModal';
 
 type BoardColumnTaskProps = {
   id: string;
   columnId: string;
   task: Task;
   onTaskDeleted: (taskId: string, columnId: string) => void;
+  onTaskEdited: (taskId: string, task: Task) => void;
 };
 
 const subtaskPercent = (subtasks: Subtask[]) => {
@@ -15,43 +18,68 @@ const subtaskPercent = (subtasks: Subtask[]) => {
   return Math.round((doneCount / subtasks.length) * 100);
 };
 
-export const BoardColumnTask = ({ id, columnId, task, onTaskDeleted }: BoardColumnTaskProps) => {
+export const BoardColumnTask = ({
+  id,
+  columnId,
+  task,
+  onTaskDeleted,
+  onTaskEdited
+}: BoardColumnTaskProps) => {
   const { name, subtasks } = task;
+  const [isTaskOverviewModalOpen, setIsTaskOverviewModalOpen] = useState(false);
+
   return (
-    <Card
-      style={{ border: '1px solid #cfcfcf', maxWidth: '100%' }}
-      bodyStyle={{ margin: '0px', padding: '8px', textAlign: 'center' }}
-      actions={[
-        <Tooltip key='edit' title='Delete Column'>
-          <EditOutlined onClick={() => console.log(`editing task ${id}`)} />
-        </Tooltip>,
-        <Tooltip key='delete' title='Delete Column'>
-          <DeleteOutlined onClick={() => onTaskDeleted(id, columnId)} />
-        </Tooltip>
-      ]}
-    >
-      <Space direction='vertical'>
-        <h1 style={{ margin: '0', padding: '0' }}>{name}</h1>
-        {subtasks?.length > 0 && (
-          <div>
-            <Row>
-              <Col>
-                <Progress
-                  percent={subtaskPercent(subtasks)}
-                  showInfo={false}
-                  status='active'
-                  style={{ width: '100px' }}
-                />
-              </Col>
-              <Col>
-                <span>{`${subtasks.filter(subtask => subtask.done).length} / ${
-                  subtasks.length
-                }`}</span>
-              </Col>
-            </Row>
-          </div>
-        )}
-      </Space>
-    </Card>
+    <>
+      <TaskOverviewModal
+        isOpen={isTaskOverviewModalOpen}
+        close={() => setIsTaskOverviewModalOpen(false)}
+        task={task}
+      />
+      <Card
+        style={{ border: '1px solid #cfcfcf', maxWidth: '100%' }}
+        bodyStyle={{ margin: '0px', padding: '8px', textAlign: 'center' }}
+        actions={[
+          <Tooltip key='edit' placement='bottom' title='Edit Task'>
+            <EditOutlined onClick={() => setIsTaskOverviewModalOpen(true)} />
+          </Tooltip>,
+          <Tooltip key='delete' placement='bottom' title='Delete Task'>
+            <Popconfirm
+              title='Delete Task'
+              description='Are you sure you want to delete this task?'
+              icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+              okButtonProps={{ danger: true }}
+              okText='Yes'
+              cancelText='No'
+              onConfirm={() => onTaskDeleted(id, columnId)}
+            >
+              <DeleteOutlined />
+            </Popconfirm>
+          </Tooltip>
+        ]}
+      >
+        <Space direction='vertical'>
+          <h1 style={{ margin: '0', padding: '0' }}>{name}</h1>
+          {subtasks?.length > 0 && (
+            <div>
+              <Row>
+                <Col>
+                  <Progress
+                    percent={subtaskPercent(subtasks)}
+                    showInfo={false}
+                    status='active'
+                    style={{ width: '100px' }}
+                  />
+                </Col>
+                <Col>
+                  <span>{`${subtasks.filter(subtask => subtask.done).length} / ${
+                    subtasks.length
+                  }`}</span>
+                </Col>
+              </Row>
+            </div>
+          )}
+        </Space>
+      </Card>
+    </>
   );
 };
