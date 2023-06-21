f5fed0980830ff9042c3334c4632ab681107fd62
Leander Steiner <leandersteiner@yahoo.de>
2023-06-21 11:48:44 +0200

    #25: udpated date utils

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,21 +1,52 @@
-export interface Timestamp {
-  created_at: string;
+export interface CreatedTimestamp {
+  createdAt: string;
+}
+
+export interface UpdatedTimestamp {
+  updatedAt: string;
+}
+
+export interface TimestampedEntity extends CreatedTimestamp, UpdatedTimestamp {}
+
+export interface DatedEntity {
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const toDateString = (dateString: string): string => {
   return new Date(dateString).toLocaleDateString('de-DE');
 };
 
-const sortByDateAsc = <T extends Timestamp>(arr: T[]): T[] => {
-  return arr.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
+const toDate = (dateString: string): Date => {
+  return new Date(dateString);
 };
 
-const sortByDateDesc = <T extends Timestamp>(arr: T[]): T[] => {
-  return arr.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+const setDates = <T extends TimestampedEntity>(entity: T): DatedEntity => {
+  return { ...entity, createdAt: toDate(entity.createdAt), updatedAt: toDate(entity.updatedAt) };
+};
+
+const sortByCreationDateAsc = <T extends CreatedTimestamp>(arr: T[]): T[] => {
+  return arr.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+};
+
+const sortByCreationDateDesc = <T extends CreatedTimestamp>(arr: T[]): T[] => {
+  return arr.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+};
+
+const sortByUpdateDateAsc = <T extends UpdatedTimestamp>(arr: T[]): T[] => {
+  return arr.sort((a, b) => new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime());
+};
+
+const sortByUpdateDateDesc = <T extends UpdatedTimestamp>(arr: T[]): T[] => {
+  return arr.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
 };
 
 export const DateUtils = {
+  toDate,
+  setDates,
   toDateString,
-  sortByDateAsc,
-  sortByDateDesc
+  sortByCreationDateAsc,
+  sortByCreationDateDesc,
+  sortByUpdateDateAsc,
+  sortByUpdateDateDesc
 };
