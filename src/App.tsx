18e25dd8e8297a6dbcc93acce11d53769f340acc
Leander Steiner <leandersteiner@yahoo.de>
2023-06-21 22:09:14 +0200

    #25: updated router and side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Overview } from './components/layout/Overview';
 import { PathContextProvider } from './context/PathContext';
 import { HomePage } from './views/home/HomePage';
-import { RegistrationForm } from './components/auth/RegistrationForm';
-import { LoginForm } from './components/auth/LoginForm';
-import { Logout } from './components/auth/Logout';
-import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import { Profile } from './views/user/Profile';
 import { BoardView } from './views/board/BoardView';
 import { Settings } from './views/settings/Settings';
@@ -16,11 +12,14 @@ import { Projects } from './views/projects/Projects';
 import { NotFound } from './views/NotFound';
 import 'easymde/dist/easymde.min.css';
 import 'highlight.js/styles/a11y-light.css';
+import { LoginForm } from './components/auth/LoginForm';
+import { RegistrationForm } from './components/auth/RegistrationForm';
+import { Logout } from './components/auth/Logout';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: false,
+      retry: 1,
       refetchOnWindowFocus: true,
       useErrorBoundary: true
     }
@@ -33,47 +32,21 @@ export const App: React.FC = () => {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Navigate to='home' />} />
+            <Route path='/' element={<Overview />}>
+              <Route path='home' element={<HomePage />} />
+              <Route path='profile' element={<Profile />} />
+              <Route path='projects' element={<Projects />} />
+              <Route path='orgs' element={<Organisations />} />
+              <Route
+                path='orgs/:orgId/teams/:teamId/projects/:projectId/board'
+                element={<BoardView />}
+              />
+              <Route path='settings' element={<Settings />} />
+            </Route>
             <Route path='/auth' element={<Overview />}>
               <Route path='login' element={<LoginForm />} />
               <Route path='register' element={<RegistrationForm />} />
               <Route path='logout' element={<Logout />} />
-            </Route>
-            <Route path='/user' element={<Overview />}>
-              <Route path='' element={<Profile />} />
-            </Route>
-            <Route path='/home' element={<Overview />}>
-              <Route path='' element={<HomePage />} />
-            </Route>
-            <Route path='projects' element={<Overview />}>
-              <Route path='' element={<Projects />} />
-            </Route>
-            <Route path='orgs' element={<Overview />}>
-              <Route path='' element={<Organisations />} />
-            </Route>
-            <Route
-              path='orgs/:orgid/teams/:teamid/projects/:projectid/board'
-              element={<Overview />}
-            >
-              <Route path='' element={<Settings />} />
-            </Route>
-            <Route path='settings' element={<Overview />}>
-              <Route path='' element={<Settings />} />
-              <Route
-                index
-                element={
-                  <ProtectedRoute>
-                    <HomePage />
-                  </ProtectedRoute>
-                }
-              />
-            </Route>
-            <Route path='/orgs/:orgId/teams/:teamId/projects/:projectId' element={<Overview />}>
-              <Route path='board' element={<BoardView />} />
-              <Route path='projects' element={<Projects />} />
-              <Route path='projects/:projectId/boards' element={<Settings />} />
-              <Route path='settings' element={<Settings />} />
-              <Route path='orgs' element={<Organisations />} />
             </Route>
             <Route path='*' element={<Overview />}>
               <Route path='*' element={<NotFound />} />
