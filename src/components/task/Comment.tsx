3ce2dd939e4ce933bb8b981a4ad3f4a5cdda0b70
Leander Steiner <leandersteiner@yahoo.de>
2023-06-21 17:09:56 +0200

    #25: implemented components to show comments

--- src/components/task/Comment.tsx
+++ src/components/task/Comment.tsx
@@ -1,3 +1,87 @@
-export const Comment = () => {
-  return <h2>Comment</h2>;
+import { Avatar, Card, Col, Divider, Popconfirm, Row, Space, Tooltip, Typography } from 'antd';
+import { DeleteOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
+import Meta from 'antd/es/card/Meta';
+import { Navigate } from 'react-router-dom';
+import Paragraph from 'antd/es/typography/Paragraph';
+import { TaskComment } from '../../models/task/TaskComment';
+import { useAuth } from '../../context/AuthContext';
+
+const { Text } = Typography;
+
+type CommentProps = {
+  comment: TaskComment;
+  onCommentDeleted: (commentId: string) => void;
+  onCommentUpdated: (commentId: string, comment: TaskComment) => void;
+};
+
+export const Comment = ({ comment, onCommentDeleted, onCommentUpdated }: CommentProps) => {
+  const { user } = useAuth();
+
+  if (!user) return <Navigate to='/login' />;
+
+  const isCreator = user.id === comment.creator.id;
+  comment.createdAt = new Date(comment.createdAt);
+  return (
+    <Card
+      style={{ marginTop: 16 }}
+      actions={
+        isCreator
+          ? [
+              <Tooltip key='delete' placement='bottom' title='Delete Task'>
+                <Popconfirm
+                  title='Delete Task'
+                  description='Are you sure you want to delete this task?'
+                  icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+                  okButtonProps={{ danger: true }}
+                  okText='Yes'
+                  cancelText='No'
+                  onConfirm={() => onCommentDeleted(comment.id)}
+                >
+                  <DeleteOutlined />
+                </Popconfirm>
+              </Tooltip>
+            ]
+          : []
+      }
+    >
+      <Meta
+        title={
+          <Row justify='space-between'>
+            <Col>
+              <Avatar
+                style={{ backgroundColor: '#87d068', marginRight: '8px' }}
+                icon={<UserOutlined />}
+              />
+              <Text>{`${comment.creator.username}`}</Text>
+            </Col>
+            <Col>
+              <Text disabled>{`${comment.createdAt.toDateString()}`}</Text>
+            </Col>
+          </Row>
+        }
+        description={
+          <Space direction='vertical' style={{ width: '100%' }}>
+            <Divider type='horizontal' style={{ margin: 0, backgroundColor: '#f9f9f9' }} />
+            <Paragraph
+              editable={
+                isCreator
+                  ? {
+                      triggerType: ['text'],
+                      onChange: text => {
+                        comment.comment = text;
+                      },
+                      onEnd: () => {
+                        onCommentUpdated(comment.id, comment);
+                      }
+                    }
+                  : false
+              }
+            >
+              {comment.comment}
+            </Paragraph>
+          </Space>
+        }
+      />
+    </Card>
+  );
 };
--- src/components/task/CommentList.tsx
+++ src/components/task/CommentList.tsx
@@ -1,15 +1,37 @@
 import Title from 'antd/es/typography/Title';
+import { Divider } from 'antd';
 import { Comment } from './Comment';
+import { TaskComment } from '../../models/task/TaskComment';
+import { AddNewItem } from '../board/AddNewItem';
 
-export const CommentList = () => {
+type CommentListProps = {
+  comments: TaskComment[];
+  onCommentCreated: (comment: string) => void;
+  onCommentDeleted: (commentId: string) => void;
+  onCommentUpdated: (commentId: string, comment: TaskComment) => void;
+};
+
+export const CommentList = ({
+  comments,
+  onCommentCreated,
+  onCommentDeleted,
+  onCommentUpdated
+}: CommentListProps) => {
   return (
     <>
-      <Title level={4} style={{ marginTop: 0 }}>
-        CommentList
-      </Title>
-      <Comment />
-      <Comment />
-      <Comment />
+      <Title level={4}>Comments</Title>
+      {comments
+        ?.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        .map(comment => (
+          <Comment
+            key={comment.id}
+            comment={comment}
+            onCommentDeleted={onCommentDeleted}
+            onCommentUpdated={onCommentUpdated}
+          />
+        ))}
+      <Divider />
+      <AddNewItem onAdd={onCommentCreated} toggleButtonText='Add Comment' />
     </>
   );
 };
