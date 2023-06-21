066875923a459d837cb896915b150147ed010386
Leander Steiner <leandersteiner@yahoo.de>
2023-06-21 17:07:31 +0200

    #25: updated useFocus hook to use ant inputref

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from 'react';
+import { InputRef } from 'antd';
 
 export const useFocus = () => {
-  const ref = useRef<HTMLInputElement>(null);
+  const ref = useRef<InputRef>(null);
 
   useEffect(() => {
     ref.current?.focus();
