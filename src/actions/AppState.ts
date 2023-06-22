26373b5abf7ef3c89ac31fc33f65b807ec732f9c
Leander Steiner <leandersteiner@yahoo.de>
2023-06-22 09:45:10 +0200

    chore: removed unused files

--- src/actions/AppState.ts
+++
@@ -1,9 +0,0 @@
-import { Board } from '../models/board/Board';
-
-export interface AppState {
-  userId: string;
-  organisationId: string;
-  teamId: string;
-  projectId: string;
-  projectBoard: Board;
-}
